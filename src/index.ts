cb9dc09b28ffadc53a7a435558c6aff7304e54bb
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2022-11-11 20:35:53 -0300

    User

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import express from "express"
-
+import express, {Request, Response} from 'express';
 import cors from 'cors'
 
+
 const app = express()
-
 app.use(express.json())
-
 app.use(cors())
 
 
