319409d2e32ee56a017761e3f8c5843ea8b9d30a
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-11-18 23:02:44 -0300

    Labebank1

---
+++ src/data.ts
@@ -0,0 +1,70 @@
+import * as allTypes from './types'
+
+export let users: allTypes.Users[] = [
+    {
+        id: 1,
+        name: "Inacio Lula da Silva",
+        dob: "25/10/1950",
+        cpf: "125.889.665-13",
+        type: "CHECKING",
+        balance: 0
+    },
+    {
+        id: 2,
+        name: "Marina Silva",
+        dob: "4/05/1960",
+        cpf: "854.556.884-25",
+        type: "SAVINGS",
+        balance: 0
+    },
+    {
+        id: 3,
+        name: "Ciro Gomes",
+        dob: "25/10/1950",
+        cpf: "158.564.111-12",
+        type: "CHECKING",
+        balance: 0
+    },
+    {
+        id: 4,
+        name: "Jair Embora Messias Bolsonaro",
+        dob: "25/10/1950",
+        cpf: "666.666.666-22",
+        type:"SAVINGS",
+        balance: 0
+    },
+    {
+        id: 5,
+        name: "Padre Kelmon Kelvin",
+        dob: "25/10/1950",
+        cpf: "584.999.456-58",
+        type: "CHECKING",
+        balance: 0
+    },
+    {
+        id: 6,
+        name: "Soraya da Silva ",
+        dob: "25/10/1950",
+        cpf: "658.456.222-52",
+        type: "SAVINGS",
+        balance: 0
+    },
+
+    {
+        id: 7,
+        name: "Dilma Rousseff",
+        dob: "25/10/1950",
+        cpf: "512.589.444-13",
+        type: "CHECKING",
+        balance: 0
+    },
+    {
+        id: 8,
+        name: "Simone Tebet",
+        dob: "25/10/1950",
+        cpf: "548.254.589-55",
+        type: "CHECKING",
+        balance: 0
+    },
+
+]
